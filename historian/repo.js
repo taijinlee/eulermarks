66ac996097990f04f2a5451f0d3b53b17fdf355b
Tai-Jin Lee <taijin@gmail.com>
2012-08-30 18:41:48 -0700

    when adding repo, auto fetch all the files and add into queue

--- historian/repo.js
+++ historian/repo.js
@@ -1,9 +1,39 @@
 
 module.exports = function(store) {
+  var async = require('async');
+  var github = require(process.env.APP_ROOT + '/github/github.js')();
+
   var RepoModel = require(process.env.APP_ROOT + '/models/repo.js')(store);
+  var BenchmarkRunQueue = require(process.env.APP_ROOT + '/models/benchmarkRunQueue.js')(store);
 
   var create = function(repoData, callback) {
-    return new RepoModel(repoData).create(callback);
+    async.auto({
+      createRepo: function(done) {
+        return new RepoModel(repoData).create(done);
+      },
+      queueBenchmarks: function(done) {
+        github.getFiles(repoData.userId, repoData.name, function(error, files) {
+          if (error) { return done(error); }
+          async.forEach(files, function(file, eachDone) {
+            if (file.type !== 'blob') { return eachDone(); }
+
+            var runQueue = {
+              id: store.generateId(),
+              repoId: repoData.id,
+              userId: repoData.userId,
+              repo: repoData.name,
+              filename: file.path,
+              sha: file.sha
+            };
+            console.log(runQueue);
+            new BenchmarkRunQueue(runQueue).create(eachDone);
+          }, done);
+        });
+      }
+    }, function(error, results) {
+      if (error) { return callback(error); }
+      return callback(null);
+    });
   };
 
   var update = function(criteria, params, callback) {
