b11bc0d0c9e3d4a93367ce387348fac296ef3eb9
Tai-Jin Lee <taijin@gmail.com>
2012-09-14 19:26:44 -0700

    removing extra console.log

--- config/config.js
+++ config/config.js
@@ -2,8 +2,6 @@ module.exports = function() {
 
   var config = require('config');
 
-  console.log(config.app.host);
-
   config.makeHidden(config.githubOauth, 'secret');
   config.makeHidden(config.app, 'port');
   config.makeHidden(config.benchmarker, 'port');
