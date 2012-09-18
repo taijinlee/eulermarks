d8876c8d1f42563462eff189ff89a3ae5841dc49
Tai-Jin Lee <taijin@gmail.com>
2012-09-17 19:35:45 -0700

    removing caching. this screws stuff up #28

--- models/util/backbone-server.js
+++ models/util/backbone-server.js
@@ -4,8 +4,6 @@ define([
   'backbone-web-real'
 ], function(_, BackboneWebModel) {
 
-  var cache = require.nodeRequire(process.env.APP_ROOT + '/cache/cache.js')();
-
   var BackboneServerModel = BackboneWebModel.extend({
 
     create: function(callback) {
@@ -28,27 +26,23 @@ define([
 
     retrieve: function(callback) {
       var self = this;
-      var params = { criteria: this.toJSON({ defaults: false }), context: this.context, options: {} };
+      this.store.retrieve(this.toJSON({ defaults: false }), this.context, {}, function(error, item) {
+        if (!callback) { return; }
+        if (error) { return callback(error); }
+        if (!item) { return callback(null, false); }
 
-      cache.get(params, function(error, cachedValue) {
-        if (cachedValue !== null) { return callback(null, cachedValue); }
+        // populate defaults when retrieving from store
+        var parsedItem = self.parse(_.extend({}, self.getSchemaDefaults(), item));
 
-        self.store.retrieve(params.criteria, params.context, params.options, function(error, item) {
-          if (error) { return callback(error); }
-          if (!item) { return cache.set(params, false, callback); }
-          // populate defaults when retrieving from store
-          var parsedItem = self.parse(_.extend({}, self.getSchemaDefaults(), item));
-
-          var success = self.set(parsedItem, {
-            error: function(model, error) {
-              return callback(error);
-            }
-          });
-          if (!success) { return; } // error case taken care by error callback
-          return cache.set(params, self.toJSON(), callback);
+        var success = self.set(parsedItem, {
+          error: function(model, error) {
+            return callback(error);
+          }
         });
-      });
+        if (!success) { return; } // error case taken care by error callback
 
+        return callback(null, self.toJSON());
+      });
     },
 
     update: function(criteria, callback) {
@@ -76,16 +70,10 @@ define([
     },
 
     list: function(criteria, limit, skip, callback) {
-      var self = this;
-      var params = { criteria: criteria, context: this.context, options: { limit: limit, skip: skip, hint: null, raw: false, slaveOk: false } };
-
-      cache.get(params, function(error, cachedValue) {
-        if (cachedValue !== null) { return callback(null, cachedValue); }
-
-        self.store.query(params.criteria, params.context, params.options, function(error, items) {
-          if (error) { return callback(error); }
-          return cache.set(params, items, callback);
-        });
+      this.store.query(criteria, this.context, {limit: limit, skip: skip }, function(error, items) {
+        if (!callback) { return; }
+        if (error) { return callback(error); }
+        return callback(null, items);
       });
     }
 
